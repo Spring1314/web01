7d0930ba2b9d1982778e3129a28430e06019c942
lile <1429801786@qq.com>
2020-03-23 14:49:14 +0800

    Initial commit

---
+++ src/com/atguigu/User.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author Administrator
+ * @create 2020-03-23 14:40
+ */
+public class User {
+
+    public static void main(String[] args) {
+        System.out.println("如果爱情重新来过，你会不会爱我");
+    }
+}
